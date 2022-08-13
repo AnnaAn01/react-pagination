21d3dd227346537d8a9b0ae3b6db363eb379a725
Anna An <arzumanyan.an@gmail.com>
2022-08-13 09:27:02 +0400

    adding pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // making the reqest
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPage = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPage, indexOfLastPost);
+
   return (
-    <div className="App">
-      <h1>React Pagination</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">React Pagination</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
